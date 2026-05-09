e1a212ad2986424f6781a9d380355632331e21e3
BartlomiejSzkodny <szkobart@outlook.com>
2026-05-09 23:26:01 +0200

    sipo and piso

--- verilogprojectfile/TOP_MODULE.sv
+++ verilogprojectfile/TOP_MODULE.sv
@@ -5,11 +5,7 @@ output [9:0] l
 
 );
 //register4biten inst_reg(.clk(p[0]),.en(p[1]),.d(p[5:2]),.q(l[3:0]));
-shiftregister ins_shiftreg(
-.clk(b[0]),
-.en(p[1]),
-.r(l[7:0]),
-.si(p[2]),
-.so(l[8])
-);
+//shiftregister ins_shiftreg(.clk(b[0]),.en(p[1]),.r(l[7:0]),.si(p[2]),.so(l[8]));
+//registersipo inssipo(.clk(p[0]),.rst(p[1]),.ser(p[2]),.ou(l[7:0]));
+//registerPISO pisoInst(.dat(p[8:1]),.clk(b[0]),.loadshift(p[9]),.leds(l[7:0]),.ou(l[9]));
 endmodule
